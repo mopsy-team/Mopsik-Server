15ae339976a7257a179964c68bad255bc28e1ed3
oroko123 <pperkowski96@gmail.com>
2018-03-22 04:37:00 +0100

    Format of API calls is not a list, but a dict, like it was earlier

--- mops_api/views.py
+++ mops_api/views.py
@@ -7,10 +7,26 @@ from mops_api.serializers import MOPSerializer, TakenSerializer
 
 
 class MOPViewSet(viewsets.ModelViewSet):
+    def list(self, request, *args, **kwargs):
+        response = super(MOPViewSet, self).list(request, *args, **kwargs)
+        x = {}
+        for i, mop in enumerate(response.data, 1):
+            x[i] = mop
+        response.data = x
+        return response
+
     queryset = MOP.objects.all().order_by('id')
     serializer_class = MOPSerializer
 
 
 class TakenViewSet(viewsets.ModelViewSet):
+    def list(self, request, *args, **kwargs):
+        response = super(TakenViewSet, self).list(request, *args, **kwargs)
+        x = {}
+        for i, mop in enumerate(response.data, 1):
+            x[i] = mop
+        response.data = x
+        return response
+
     queryset = MOP.objects.all().order_by('id')
     serializer_class = TakenSerializer
