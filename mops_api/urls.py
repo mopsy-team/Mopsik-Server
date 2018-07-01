ef6a0128813cd37f4d84082032f2ccdf12d1cd44
oroko123 <pperkowski96@gmail.com>
2018-07-01 23:01:25 +0200

    Added parsing urls

--- mops_api/urls.py
+++ mops_api/urls.py
@@ -8,11 +8,15 @@ from mops_api import views
 router = routers.DefaultRouter()
 router.register(r'mops', views.MOPViewSet, base_name='mops')
 router.register(r'taken', views.TakenViewSet, base_name='taken')
+#router.register(r'wsg_to_92', views.wgs_to_puwg92, base_name='wgs_to_puwg92')
+#router.register(r'92_to_wsg', views.puwg92_to_wgs84, base_name='puwg92_to_wgs')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'wsg_to_92', views.wgs_to_puwg92),
+    url(r'92_to_wsg', views.puwg92_to_wgs),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
